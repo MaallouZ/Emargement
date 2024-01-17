dba168bea86b3b8c073fe471b4486b80cfaf3629
MaallouZ <mehdi.maallou@stjosup.com>
2024-01-17 14:54:36 +0100

    final database features

--- emargement.sql
+++ emargement.sql
@@ -3,13 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 13 jan. 2024 à 14:59
+-- Généré le : mer. 17 jan. 2024 à 13:40
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+01:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -31,8 +31,12 @@ DROP TABLE IF EXISTS `activite`;
 CREATE TABLE IF NOT EXISTS `activite` (
   `idActivite` int NOT NULL AUTO_INCREMENT,
   `libelleActivite` varchar(50) NOT NULL,
+  `dateDebuteActivite` date DEFAULT NULL,
+  `dateFinActivite` date DEFAULT NULL,
   PRIMARY KEY (`idActivite`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 
 -- --------------------------------------------------------
 
@@ -51,7 +55,9 @@ CREATE TABLE IF NOT EXISTS `estpresent` (
   KEY `IDvisiteur` (`IDvisiteur`),
   KEY `idJournee` (`idJournee`),
   KEY `idActivite` (`idActivite`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 
 -- --------------------------------------------------------
 
@@ -64,16 +70,19 @@ CREATE TABLE IF NOT EXISTS `journée` (
   `idJournee` int NOT NULL AUTO_INCREMENT,
   `dateJournee` date NOT NULL,
   PRIMARY KEY (`idJournee`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 --
 -- Déclencheurs `journée`
 --
 DROP TRIGGER IF EXISTS `newPresence`;
 DELIMITER $$
-CREATE TRIGGER `newPresence` AFTER INSERT ON `journée` FOR EACH ROW INSERT INTO estpresent (IDvisiteur, idJournee, Present)
-SELECT visiteur.ID, NEW.idJournee, FALSE
-FROM visiteur
+CREATE TRIGGER `newPresence` AFTER INSERT ON `journée` FOR EACH ROW INSERT INTO estPresent (estpresent.idJournee, estpresent.IDvisiteur, estpresent.idActivite,estpresent.present)
+SELECT NEW.idJournee, visiteur.IDvisiteur, activite.idActivite, FALSE
+FROM visiteur, activite
+WHERE activite.dateDebuteActivite <= NEW.dateJournee
+AND activite.dateFinActivite >= NEW.dateJournee
 $$
 DELIMITER ;
 
@@ -92,16 +101,9 @@ CREATE TABLE IF NOT EXISTS `visiteur` (
   `ville` varchar(50) NOT NULL,
   `ADH` tinyint(1) NOT NULL DEFAULT '0',
   `tel` varchar(20) DEFAULT NULL,
+  `sexe` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`IDvisiteur`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
-
---
--- Déchargement des données de la table `visiteur`
---
-
-INSERT INTO `visiteur` (`IDvisiteur`, `nom`, `prenom`, `age`, `ville`, `ADH`, `tel`) VALUES
-(11, 'MAALLOU', 'Mehdi', 22, 'Lillebonne', 0, NULL),
-(12, 'LOISON', 'Morgan', 19, 'Rouville', 0, NULL);
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Contraintes pour les tables déchargées
@@ -120,8 +122,8 @@ DELIMITER $$
 -- Évènements
 --
 DROP EVENT IF EXISTS `addDay`$$
-CREATE DEFINER=`root`@`localhost` EVENT `addDay` ON SCHEDULE EVERY 1 DAY STARTS '2024-01-11 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO INSERT INTO journée (dateJournee)
-VALUES(NOW())$$
+CREATE DEFINER=`root`@`localhost` EVENT `addDay` ON SCHEDULE EVERY 1 DAY STARTS '2024-01-17 12:00:00' ON COMPLETION NOT PRESERVE ENABLE DO INSERT INTO journée (dateJournee)
+  VALUES (NOW())$$
 
 DELIMITER ;
 COMMIT;
