eedc7060c0af2bd847428c7594f37c4f35c288a5
MaallouZ <mehdi.maallou@stjosup.com>
2024-01-17 16:21:16 +0100

    add data printer function

--- util.php
+++ util.php
@@ -8,12 +8,31 @@ function addDay($conn){
     $stmt->execute();
     $lastday = $stmt -> fetch()[0];
     $today = date('Y-m-d');
-
+    
+    
     if($today != $lastday){
         // On ajoute une journée dans la base de données
-        $sql = "INSERT INTO journée (dateJournee) VALUES (". $today .");";
+        $sql = "INSERT INTO journée (dateJournee) VALUES (NOW());";
         $stmt= $conn -> prepare($sql);
         $stmt->execute();
     }
 }
+
+function printEmarg($conn){
+    $sql = "SELECT nom, prenom, age, sexe, ADH, ville, tel, present FROM visiteur INNER JOIN estpresent ON estpresent.IDvisiteur = visiteur.IDvisiteur WHERE estpresent.idActivite = 1;";
+    $stmt = $conn -> prepare($sql);
+    $stmt -> execute();
+
+    $ndata = $stmt -> rowCount();
+    $data = $stmt -> fetchAll();
+
+    for ($i = 0; $i < $ndata; $i++) {
+        echo "<tr>";
+        for ($j=0; $j < 8 ; $j++) { 
+           echo("<td>" . $data[$i][$j] . "</td>");
+        }
+        echo "</tr>";
+    }
+}
 ?>
+
