60a9e62c0a954b21e9b62a52ed99186e55b96f9a
MaallouZ <120006092+MaallouZ@users.noreply.github.com>
2024-04-15 14:57:40 +0200

    remove topbar

--- visiteurs.php
+++ visiteurs.php
@@ -98,28 +98,6 @@ $jsonData = json_encode($data);
             <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </nav>
-                <!-- End of Topbar -->
-
                 <!-- Tableau d'appel -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
