14954f15cb7ee9fe02b55d4acb919545ab426de4
Mehdi Maallou <maalloumehdi@gmail.com>
2024-09-25 15:33:52 +0200

    debug stat features

--- loadData.php
+++ loadData.php
@@ -18,14 +18,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             AND journée.dateJournee BETWEEN  :dateDebut AND :dateFin;';
 
         $stmt = $conn->prepare($sqlTotalVisiteur);
-        $stmt ->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
-        $stmt ->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
-        $stmt ->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
+        $stmt->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
+        $stmt->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
+        $stmt->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
         $stmt->execute();
 
         $totalVisiteur = $stmt->fetch();
 
-        // Total d'hommes et de femmes
+        // Total d'hommes
         $sqlHF = "SELECT visiteur.sexe, COUNT(DISTINCT estPresent.IDvisiteur)
             FROM estPresent
             INNER JOIN visiteur ON estPresent.IDvisiteur = visiteur.IDvisiteur
@@ -33,15 +33,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             WHERE idActivite = :idActivite
             AND present = 1
             AND journée.dateJournee BETWEEN :dateDebut AND :dateFin
-            GROUP BY visiteur.sexe;";
+            AND visiteur.sexe = 'M';";
 
-        $stmt2 = $conn->prepare($sqlHF);
-        $stmt2->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
-        $stmt2->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
-        $stmt2->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
-        $stmt2->execute();
+        $stmt = $conn->prepare($sqlHF);
+        $stmt->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
+        $stmt->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
+        $stmt->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
+        $stmt->execute();
 
-        $total_HF = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        $total_H = $stmt->fetch();
+
+        // Total de femmes
+        $sqlHF = "SELECT visiteur.sexe, COUNT(DISTINCT estPresent.IDvisiteur)
+                FROM estPresent
+                INNER JOIN visiteur ON estPresent.IDvisiteur = visiteur.IDvisiteur
+                INNER JOIN journée ON estPresent.idJournee = journée.idJournee
+                WHERE idActivite = :idActivite
+                AND present = 1
+                AND journée.dateJournee BETWEEN :dateDebut AND :dateFin
+                AND visiteur.sexe = 'F';";
+
+        $stmt = $conn->prepare($sqlHF);
+        $stmt->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
+        $stmt->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
+        $stmt->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $total_F = $stmt->fetch();
 
         //Fréquentation sur la période
         $sqlFrequentation = "SELECT journée.dateJournee, COUNT(*)
@@ -52,13 +70,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             AND journée.dateJournee BETWEEN :dateDebut AND :dateFin
             GROUP BY journée.dateJournee;";
 
-        $stmt3 = $conn->prepare($sqlFrequentation);
-        $stmt3->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
-        $stmt3->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
-        $stmt3->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
-        $stmt3->execute();
+        $stmt = $conn->prepare($sqlFrequentation);
+        $stmt->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
+        $stmt->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
+        $stmt->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
+        $stmt->execute();
 
-        $frequentation = $stmt3->fetchAll();
+        $frequentation = $stmt->fetchAll();
 
         //Moyenne visiteur/jour
         $sqlMoyenne = "SELECT AVG(nb_present) AS moyenne_presences FROM (
@@ -71,37 +89,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             GROUP BY journée.dateJournee
                         ) AS t;";
 
-        $stmt4 = $conn -> prepare($sqlMoyenne);
-        $stmt4->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
-        $stmt4->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
-        $stmt4->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
-        $stmt4->execute();
+        $stmt = $conn->prepare($sqlMoyenne);
+        $stmt->bindParam(":idActivite", $idActivite, PDO::PARAM_INT);
+        $stmt->bindParam(":dateDebut", $dateDebut, PDO::PARAM_STR);
+        $stmt->bindParam(":dateFin", $dateFin, PDO::PARAM_STR);
+        $stmt->execute();
 
-        $moyenne = $stmt4-> fetch();
+        $moyenne = $stmt->fetch();
 
         $response = [
             "status" => "success",
             "totalVisiteur" => $totalVisiteur[0],
-            "total_H" => $total_HF[1]["COUNT(DISTINCT estPresent.IDvisiteur)"],
-            "total_F" => $total_HF[0]["COUNT(DISTINCT estPresent.IDvisiteur)"],
+            "total_H" => $total_H[1],
+            "total_F" => $total_F[1],
             "frequentation" => $frequentation,
             "moyenne" => floatval($moyenne[0])
         ];
 
         echo json_encode($response);
-
     } catch (PDOException $e) {
         echo json_encode([
             "status" => "error",
             "message" => "Erreur lors de la connexion à la base de données: " . $e->getMessage()
         ]);
     }
-}
-else{
+} else {
     echo json_encode([
         "status" => "error",
         "message" => "Requête non autorisée. Utilisez la méthode POST."
     ]);
     exit();
 }
-?>
